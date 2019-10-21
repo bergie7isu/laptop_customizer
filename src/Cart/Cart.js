77a1c892d6ec1bc15ba4bf03ea180615877aa99e
Mark Bergstrom <markbergstrom@gmail.com>
2019-10-20 20:25:20 -0500

    cart list created as component

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import CartItem from '../CartItem/CartItem';
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -7,20 +8,7 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 
 class Cart extends Component {
     render() {
-        const summary = Object.keys(this.props.selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const selectedOption = this.props.selected[feature];
-      
-            return (
-                <div className="summary__option" key={featureHash}>
-                    <div className="summary__option__label">{feature}</div>
-                    <div className="summary__option__value">{selectedOption.name}</div>
-                    <div className="summary__option__cost">
-                        {USCurrencyFormat.format(selectedOption.cost)}
-                    </div>
-                </div>
-            );
-        });
+        
       
         const total = Object.keys(this.props.selected).reduce(
             (acc, curr) => acc + this.props.selected[curr].cost,
@@ -30,7 +18,17 @@ class Cart extends Component {
         return (
             <section className="main__summary">
                 <h2>Your cart</h2>
-                {summary}
+                {Object.keys(this.props.selected).map((feature, idx) =>
+                    <CartItem
+                        selected={this.props.selected}
+                        feature={feature}
+                        idx={idx}
+                        featureHash={feature + '-' + idx}
+                        selectedOption={this.props.selected[feature]}
+                    />
+                )}
+
+
                 <div className="summary__total">
                     <div className="summary__total__label">Total</div>
                     <div className="summary__total__value">
